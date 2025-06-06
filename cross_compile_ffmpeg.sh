36a2f2f36e85d23592ba71cbf792a0a61049abc3
cxumol <cxumol@users.noreply.github.com>
2025-06-06 04:37:10 -0700

    add -DENABLE_ALPHA=1 to x265 cmake params

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1900,7 +1900,7 @@ build_libx265() {
   fi
   cd $checkout_dir
 
-  local cmake_params="-DENABLE_SHARED=0" # build x265.exe
+  local cmake_params="-DENABLE_SHARED=0 -DENABLE_ALPHA=1" # build x265.exe
 
   # Apply x86 noasm detection fix on newer versions
   if [[ $x265_git_checkout_version != *"3.5"* ]] && [[ $x265_git_checkout_version != *"3.4"* ]] && [[ $x265_git_checkout_version != *"3.3"* ]] && [[ $x265_git_checkout_version != *"3.2"* ]] && [[ $x265_git_checkout_version != *"3.1"* ]]; then
